8adb75852bb92550d1531705e40747dcb3bd68f9
Surya-SAT <165343982+Surya-Sathya@users.noreply.github.com>
2024-11-25 09:57:59 +0530

    Update linked_list_implementation.py

--- Data Structure Implementations/linked_list_implementation.py
+++ Data Structure Implementations/linked_list_implementation.py
@@ -4,8 +4,24 @@
     #Delete an element at the beginning
 
 class Node():
-    def __init__(self, data) -> None: #This fnc will return nothing
+    def __init__(self, data, next) -> None: #This fnc will return nothing
         self.data = data
-        self.link = self
+        self.next = next
 
-first_node = Node(13, )
+class LinkedList():
+    def __init__(self) -> None:
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node 
+    
+    def print_linked_list(self):
+        if self.head == None:
+            print("Linked List is Empty")
+            return
+
+if __name__ == '__main__':
+    ll = LinkedList()
+    ll.print_linked_list()
+    ll.insert_at_beginning(50)
