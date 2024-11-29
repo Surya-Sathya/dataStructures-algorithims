3c0f49f974a112748c6cfc6dbcfb2f4ae9e08222
Surya-Sathya <SURYASATHYA20@GMAIL.COM>
2024-11-29 18:25:32 +1100

    Update logarithmic_time_multiply.py

--- Recursive Functions/logarithmic_time_multiply.py
+++ Recursive Functions/logarithmic_time_multiply.py
@@ -17,7 +17,7 @@ n = 11
 print(power(a, n))
 print(f"This would have been done in {n-1} multiply operators otherwise -> O(n) compard to Log(n) in recursive form")
 
-#For 2^11, we get 4 print statements. Each print statement at worst represents two multiplications (for when x is odd)
+#For 2^11, we get 4 print statements (4 recursive calls). Each print statement at worst represents two multiplications (for when x is odd)
 #The amount of recursive calls we get is how many times we can split 11 into halves, or log2(11) ~ 3.459 = 4 calls
-#Hence Hence the amount of time steps to mulitply t(n) = 2*log(n) + 2*log(n) (the amount of if statements in comparison)
-# O(4log(n)) -> O(log(n))
+#Hence Hence the amount of time steps to mulitply t(n) = 2*log(n) + (C)*log(n) (C = amount of O(1) steps in if statements/assignments per call)
+# O(2Clog(n)) -> O(log(n)), as there exists some c that bounds this t(n). Also a c2 which lower bounds it; hence O-sigma = O(logn)
