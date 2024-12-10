d470abca9efec67068bd966b2ca0c1e7c0f7cf61
Surya-Sathya <SURYASATHYA20@GMAIL.COM>
2024-12-10 11:04:58 +1100

    Small convenicence changes to log_time_multiply

--- Recursive Functions/logarithmic_time_multiply.py
+++ Recursive Functions/logarithmic_time_multiply.py
@@ -1,5 +1,3 @@
-from math import floor
-
 def power(a,n):
     if n == 0:
         return 1
@@ -10,7 +8,7 @@ def power(a,n):
     
     elif n % 2 == 1:
         print(f"One squaring (^2) operator and One Multiply Operator for {a}*(power({a}, {n}/2))**2")
-        return a*(power(a, floor(n/2)))**2
+        return a*(power(a, n//2))**2
 
 a = 2
 n = 11
@@ -21,3 +19,5 @@ print(f"This would have been done in {n-1} multiply operators otherwise -> O(n) 
 #The amount of recursive calls we get is how many times we can split 11 into halves, or log2(11) ~ 3.459 = 4 calls
 #Hence Hence the amount of time steps to mulitply t(n) = 2*log(n) + (C)*log(n) (C = amount of O(1) steps in if statements/assignments per call)
 # O(2Clog(n)) -> O(log(n)), as there exists some c that bounds this t(n). Also a c2 which lower bounds it; hence O-sigma = O(logn)
+
+#Recursive formula => a^(n) = (a^(n/2))*2
