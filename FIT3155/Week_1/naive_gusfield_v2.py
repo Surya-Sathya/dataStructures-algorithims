feb57db0b99a14988e74012a0f2ec66b15d586f0
Surya-Sathya <SURYASATHYA20@GMAIL.COM>
2025-03-17 22:18:19 +1100

    a

--- FIT3155/Week_1/naive_gusfield_v2.py
+++ FIT3155/Week_1/naive_gusfield_v2.py
@@ -1,12 +1,14 @@
 class Gusfield_Z():
     def __init__(self):
         self.comparison_counter = 0
+        self.total_comp_counter = 0
 
     def naive_string_match(self, txt: str, idx: int) -> int:
         z_score = 0
         for i in range(0, len(txt)-idx):
             #print(f"Comparing txt[{i}] with txt[{idx+i}]")
             self.comparison_counter += 1
+            self.total_comp_counter += 1
             if txt[i] == txt[idx+i]: z_score += 1
             else: 
                 #print("Mismatch!")
@@ -29,10 +31,12 @@ class Gusfield_Z():
         #Explicit comparison for Z2
         res[1] = self.naive_string_match(txt,1)
         if(res[1] > 0): l, r = 1, res[1]
+        print(f"Iteration: {1}, with a comparison count of: {1 if self.comparison_counter == 0 else self.comparison_counter}")
         #print(f"Left point of furthest box: {l} and Right point of furthest box: {r}\n")
 
 
         for k in range(2, len(txt)): 
+            print(f"Iteration: {k}", end=", with a comparison count of: ")
             #Case 1) Zk is greater than the rightmost point of the furthest z-box
             if k > r: 
                 zk_score = self.naive_string_match(txt, k)
@@ -42,27 +46,31 @@ class Gusfield_Z():
                 adj_z_score = res[k-l]
                 if adj_z_score < r-k+1: zk_score = adj_z_score
 
-                if adj_z_score > r-k+1: zk_score = r-k+1
+                elif adj_z_score > r-k+1: zk_score = r-k+1
 
-                if adj_z_score == r-k+1:
+                elif adj_z_score == r-k+1:
                     if r+1 == len(txt): zk_score = adj_z_score
                     else:
                         zk_score = self.naive_string_match(txt, r-k+1) + r-k+1
             
+            print(1 if self.comparison_counter == 0 else self.comparison_counter)
+            self.comparison_counter = 0
             res[k] = zk_score
 
             #print(f"Left point of furthest box: {l} and Right point of furthest box: {r}\n")
 
         print(res)
 
-txt = "aabcaabxaaazasdfnaabbaaaabbaabccaaababbaab"
-naive_method = Gusfield_Z()
-naive_method.quadratic_string(txt)
-print(f"Number of comparisons with naive algorithim: {naive_method.comparison_counter} \n")
+txt = "aabcaabxaabcaabcay"
+# naive_method = Gusfield_Z()
+# naive_method.quadratic_string(txt)
+# print(f"Number of comparisons with naive algorithim: {naive_method.comparison_counter} \n")
 
 gusfield_z_method = Gusfield_Z()
 gusfield_z_method.z_algorithim(txt)
-print(f"Number of comparisons with naive algorithim: {gusfield_z_method.comparison_counter}")
+print("\n", end="")
+print(f"Number of comparisons with z-algo algorithim: {gusfield_z_method.total_comp_counter}")
+print(f"Length of text = {len(txt)}")
 
 
 
